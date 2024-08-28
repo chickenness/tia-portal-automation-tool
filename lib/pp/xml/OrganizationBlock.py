d5ad6b9952a1f9b1742ea6d24af893a08c89fb8b
Wineff Chen Zhen Y. Gulane <wineffg@titusgt.com>
2024-08-28 17:49:01 +0900

    Update: changed wire xml class and renamed template strings

--- lib/pp/xml/OrganizationBlock.py
+++ lib/pp/xml/OrganizationBlock.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from pathlib import Path
 from string import Template
@@ -8,13 +9,14 @@ class XML:
     Name: str
     Number: int
     ProgrammingLanguage: str
-    Blocks: list
+    Blocks: list[SWBlocksCompileUnit]
+    MultilingualText: str
 
 @dataclass
 class SWBlocksCompileUnit:
     Id: int
-    Parts: list
-    Wires: list
+    Parts: list[Call]
+    Wires: list[Wire]
     ProgrammingLanguage: str
 
 @dataclass
@@ -25,18 +27,16 @@ class Call:
     BlockNumber: int
     InstanceUid: int
     ComponentName: str
-    DatabaseType: str
-    DatabaseBlockNumber: int
+    DataBlockType: str
+    DataBlockNumber: int
 
 @dataclass
-class WireA:
+class Wire:
     Uid: int
     EnUid: int
 
 @dataclass
-class WireB:
-    Uid: int
-    EnUid: int
+class WireB(Wire):
     EnoUid: int
 
 @dataclass
@@ -59,7 +59,7 @@ def build_xml(data: XML) -> str:
     for block in data.Blocks:
         blocks += build_sw_blocks_compile_unit(block)
 
-    return xml['XML'].substitute(NAME=data.Name, NUMBER=data.Number, PROGRAMMING_LANGUAGE=data.ProgrammingLanguage, BLOCKS=blocks)
+    return xml['XML'].substitute(NAME=data.Name, NUMBER=data.Number, PROGRAMMING_LANGUAGE=data.ProgrammingLanguage, BLOCKS=blocks, MULTILINGUAL_TEXT=data.MultilingualText)
 
 def build_sw_blocks_compile_unit(data: SWBlocksCompileUnit) -> str:
     parts = ""
@@ -68,8 +68,8 @@ def build_sw_blocks_compile_unit(data: SWBlocksCompileUnit) -> str:
 
     wires = ""
     for wire in data.Wires:
-        if isinstance(wire, WireA):
-            wires += build_wire_a(wire)
+        if isinstance(wire, Wire):
+            wires += build_wire(wire)
         if isinstance(wire, WireB):
             wires += build_wire_b(wire)
 
@@ -80,13 +80,14 @@ def build_call(data: Call) -> str:
         raise ValueError(f"UID Value must be within range: 1; 123-32767. {data.Uid} is not valid.")
 
     return xml['Call'].substitute(UID=data.Uid, NAME=data.Name, BLOCK_TYPE=data.BlockType, BLOCK_NUMBER=data.BlockNumber, INSTANCE_UID=data.InstanceUid,
-                                  COMPONENT_NAME=data.ComponentName, DB_TYPE=data.DatabaseType, DB_BLOCK_NUMBER=data.DatabaseBlockNumber)
+                                  COMPONENT_NAME=data.ComponentName, DB_TYPE=data.DataBlockType, DB_NUMBER=data.DataBlockNumber)
 
-def build_wire_a(data: WireA) -> str:
-    return xml['Wire_a'].substitute(UID=data.Uid, EN_UID=data.EnUid)
+def build_wire(data: Wire) -> str:
+    return xml['Wire'].substitute(UID=data.Uid, EN_UID=data.EnUid)
 
 def build_wire_b(data: WireB) -> str:
     return xml['Wire_b'].substitute(UID=data.Uid, EN_UID=data.EnUid, ENO_UID=data.EnoUid)
 
 def build_multilingual_text(data: MultilingualText) -> str:
     return xml['MultilingualText'].substitute(MULTILINGUAL_TEXT_ID=data.Id, MULTILINGUAL_TEXT_ITEM_ID=data.ItemId)
+
