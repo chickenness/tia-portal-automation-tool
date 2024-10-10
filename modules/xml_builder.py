4e2955da39bed3b26d58a321fd35b4f1bb3243f0
Wineff Chen Zhen Y. Gulane <wineffg@titusgt.com>
2024-10-10 15:05:54 +0900

    UPDATE: added partial FlgNet generation

--- modules/xml_builder.py
+++ modules/xml_builder.py
@@ -1,3 +1,4 @@
+from typing import Any
 import xml.etree.ElementTree as ET
 
 INTERFACE_MEMBER = [ {
@@ -88,63 +89,101 @@ class Interface:
             }
         }
 
-class PlcBlock:
-    def __init__(self) -> None:
-        self.xml = '''<?xml version="1.0" encoding="utf-8"?>
-            <Document>
-	            <SW.Blocks ID="0">
-		            <AttributeList>
-			            <Name>default</Name>
-			            <Namespace />
-			            <Number>0</Number>
-			            <ProgrammingLanguage>LAD</ProgrammingLanguage>
-		            </AttributeList>
-		            <ObjectList>
-		            </ObjectList>
-	            </SW.Blocks>
-            </Document>
-            '''        
-
-    def build(self, name: str, number: int, programming_language: str, block_type: str) -> str:
-        root = ET.fromstring(self.xml)
-        SWBlock = root.find('./SW.Blocks')
-        if SWBlock is not None:
-            SWBlock.tag = f"SW.Blocks.{block_type}"
-        AttributeList = root.find('.//AttributeList')
-        if AttributeList is not None:
-            self.set_text(AttributeList, 'Name', name)
-            self.set_text(AttributeList, 'Number', str(number))
-            self.set_text(AttributeList, 'ProgrammingLanguage', programming_language)
-
+class XML:
+    def export(self, root: ET.Element) -> str:
         return ET.tostring(root, encoding='utf-8').decode('utf-8')
 
-    def set_text(self, root: ET.Element, name: str, value: str) -> None:
-        el = root.find(name)
-        if el is not None: el.text = value
+    def update_element_texts(self, root: ET.Element, elements: dict[str, str]):
+        for name, text in elements.items():
+            el = root.find(name)
+            if el is not None: el.text = text
 
+    def update_attributes(self, root: ET.Element, element: str, attrs: dict[str, str]):
+        el = root.find(element)
+        if el is None:
+            return
+        for key, value in attrs.items():
+            el.set(key, value)
 
-def instance(id: str, programming_language: str, parts: str, wires: str) -> str:
-    xml = f'''<SW.Blocks.CompileUnit ID="{id}" CompositionName="CompileUnits">'''
-    xml += '''<AttributeList>'''
-    xml += f'''<ProgrammingLanguage>{programming_language}</ProgrammingLanguage>'''
-    xml += '''<NetworkSource>'''
-    xml += '''<FlgNet>'''
+class PlcBlock(XML):
+    def build(self, name: str, number: int, programming_language: str, block_type: str, instances: list[list[dict[str, Any]]]) -> str:
+        root = ET.fromstring("<Document />")
+        SWBlock = ET.SubElement(root, f"SW.Blocks.{block_type}")
 
-    xml += '''<Parts>'''
-    xml += f'''{parts}'''
-    xml += '''</Parts>'''
+        AttributeList = ET.SubElement(SWBlock, "AttributeList")
+        ObjectList = self.generate_object_list(SWBlock, instances, programming_language)
 
-    xml += '''<Wires>'''
-    xml += f'''{wires}'''
-    xml += '''</Wires>'''
+        ET.SubElement(AttributeList, "Name").text = name
+        ET.SubElement(AttributeList, "Number").text = str(number)
+        ET.SubElement(AttributeList, "ProgrammingLanguage").text = programming_language
 
-    xml += '''</FlgNet>'''
-    xml += '''</NetworkSource>'''
-    xml += '''</AttributeList>'''
-    xml += f'''</SW.Blocks.CompileUnit>'''
+        if block_type == 'OB':
+            ET.SubElement(AttributeList, "SecondaryType").text = "ProgramCycle"
+        
+        return self.export(root)
 
-    return xml
+    def generate_flgnet(self, calls: list[dict[str, Any]], db) -> ET.Element:
+        root = ET.fromstring("<FlgNet />")
+        Parts = ET.SubElement(root, "Parts")
+        Wires = ET.SubElement(root, "Wires")
 
-class Part:
-    def __init__(self) -> str:
-        pass
+        uid_counter = 0
+        # create Parts
+        for instance in calls:
+            Call = ET.SubElement(Parts, "Call", attrib={"UId": str(21 + uid_counter)})
+            CallInfo = ET.SubElement(Call, "CallInfo", attrib={
+                "Name": instance.get('name', 'Block_1'),
+                "BlockType": instance.get('type', 'FB'),
+            })
+            Instance = ET.SubElement(CallInfo, "Instance", attrib={
+                "Scope": "LocalVariable",
+                "UId": str(22 + uid_counter),
+            })
+            Component = ET.SubElement(Instance, "Component", attrib={"Name": "PASSIVE"})
+            # TODO: implement Parameter for Multi InstanceDB
+
+            uid_counter += 2
+
+        # create Wires
+        for i, instance in enumerate(calls):
+            Wire = ET.SubElement(Wires, "Wire", attrib={'UId': str(22 + uid_counter + i)})
+            if i == 0:
+                ET.SubElement(Wire, "OpenCon", attrib={'UId': str(21 + uid_counter)})
+                ET.SubElement(Wire, "NameCon", attrib={
+                    'UId': str(21 + i),
+                    'Name': 'en'
+                })
+                continue
+            ET.SubElement(Wire, "NameCon", attrib={
+                'UId': str(21 + (2 * i) - 2),
+                'Name': 'eno'
+            })
+            ET.SubElement(Wire, "NameCon", attrib={
+                'UId': str(21 + (2 * i)),
+                'Name': 'en'
+            })
+                
+
+        root.set('xmlns', "http://www.siemens.com/automation/Openness/SW/NetworkSource/FlgNet/v4")
+
+        return root
+
+    def generate_object_list(self, parent: ET.Element, instances: list[list[dict[str, Any]]], programming_language: str) -> ET.Element:
+        root = ET.SubElement(parent, "ObjectList")
+
+        id_counter = 0
+        for network in instances:
+            compile_unit = ET.SubElement(root, "SW.Blocks.CompileUnit", attrib={
+                "ID": format(3 + id_counter, 'X'),
+                "CompositionName": "CompileUnits",
+            })
+            AttributeList = ET.SubElement(compile_unit, "AttributeList")
+            NetworkSource = ET.SubElement(AttributeList, "NetworkSource")
+            NetworkSource.append(self.generate_flgnet(network, {}))
+
+            ET.SubElement(AttributeList, "ProgrammingLanguage").text = programming_language
+
+            id_counter += 5
+
+        return root
+
